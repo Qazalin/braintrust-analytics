9cb7e4f8b98880f7ecc8bed4deca2a823dd64517
qazalin <ghazal.nzhrzr@gmail.com>
2022-02-28 21:21:23 +0330

    bug-fix: Navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,35 @@ import { DarkModeSwitch } from './DarkModeSwitch'
 import { Options } from './Options'
 
 export const Navbar = () => {
-  return (
-    <Box width={'100%'} height="100%">
-      <Text fontSize={'1.5rem'} fontWeight={'bold'} ml="20px" mt="15px">
-        Braintrust Fee Convertor | Overview
-      </Text>
-      <Flex justifyContent="space-between" px="30px">
-        <Box mt="15px">
-          <Options
-            title="options"
-            options={[
-              'view source',
-              'about creator',
-              'Braintrust official website',
-            ]}
-          />
+    return (
+        <Box width={'100%'} height="100%">
+            <Text fontSize={'1.5rem'} fontWeight={'bold'} ml="20px" mt="15px">
+                Braintrust Fee Convertor | Overview
+            </Text>
+            <Flex justifyContent="space-between" px="30px">
+                <Box mt="15px">
+                    <Options
+                        title="options"
+                        options={[
+                            {
+                                title: 'Braintrust Official Website',
+                                link: 'https://www.usebraintrust.com/',
+                            },
+                            {
+                                title: 'View Source',
+                                link: 'https://github.com/Qazalin/braintrust-analytics',
+                            },
+                            {
+                                title: 'Fee Convertor Contract',
+                                link: 'https://etherscan.io/address/0x438e6416fe63863c434e4d6ee0c39d8f96880186#code',
+                            },
+                        ]}
+                    />
+                </Box>
+                <Box>
+                    <DarkModeSwitch isMobile={true} />
+                </Box>
+            </Flex>
         </Box>
-        <Box>
-          <DarkModeSwitch isMobile={true} />
-        </Box>
-      </Flex>
-    </Box>
-  )
+    )
 }
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,38 +1,41 @@
 import {
-  Menu,
-  MenuItem,
-  MenuButton,
-  MenuList,
-  Button,
-  useDisclosure,
+    Menu,
+    MenuItem,
+    MenuButton,
+    MenuList,
+    Button,
+    useDisclosure,
+    Link,
 } from '@chakra-ui/react'
 import { ChevronDownIcon, CloseIcon } from '@chakra-ui/icons'
 
 export const Options = ({ options, title }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
-  return (
-    <Menu isOpen={isOpen} onClose={onClose}>
-      <MenuButton
-        as={Button}
-        rightIcon={
-          isOpen ? (
-            <CloseIcon fontSize={'10px'} />
-          ) : (
-            <ChevronDownIcon fontSize={'20px'} />
-          )
-        }
-        onClick={onOpen}
-      >
-        {title}
-      </MenuButton>
-      <MenuList>
-        {options.map((option, idx) => (
-          <MenuItem textTransform={'capitalize'} key={idx}>
-            {option}
-          </MenuItem>
-        ))}
-      </MenuList>
-    </Menu>
-  )
+    return (
+        <Menu isOpen={isOpen} onClose={onClose}>
+            <MenuButton
+                as={Button}
+                rightIcon={
+                    isOpen ? (
+                        <CloseIcon fontSize={'10px'} />
+                    ) : (
+                        <ChevronDownIcon fontSize={'20px'} />
+                    )
+                }
+                onClick={onOpen}
+            >
+                {title}
+            </MenuButton>
+            <MenuList>
+                {options.map((option, idx) => (
+                    <Link key={idx} href={option.link} target="_blank">
+                        <MenuItem textTransform={'capitalize'}>
+                            {option.title}
+                        </MenuItem>
+                    </Link>
+                ))}
+            </MenuList>
+        </Menu>
+    )
 }
